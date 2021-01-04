c9575b4404fad9778e1ebac0c34f3cc6ca225a73
codebr3ak <barakaralph@gmail.com>
2021-01-04 15:30:30 +0300

    other changes

--- js/script.js
+++ js/script.js
@@ -70,11 +70,7 @@ $("#work2")
 
 //portifolio section 
 // giving feedback to the customer
-$("button").click(function () {
-  var client = document.getElementById('nameDetail').value;
-  alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
-  .preventDefault();
-});
+
 
 
 //    reset my form button
